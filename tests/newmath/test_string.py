416fdbaa227fbbe1a51d834574d3bd45c4d18553
brigith91 <168245239+brigith91@users.noreply.github.com>
2025-03-27 00:48:16 +0000

    cambios

--- tests/newmath/test_string.py
+++ tests/newmath/test_string.py
@@ -1,25 +1,40 @@
 import unittest
+import pytest 
 from newmath.string import contar_vocales,multiplicacion_vocales,porcentaje_vocales
 #python -m unittest tests/newmath/test_string.py -v para correr en consola
+# instalar biblioteca  pytest pip install pytest en consola ejecutar pytest
+# comandos pytest -h
+# informe html pip install pytest-html
+#  pytest tests/newmath/test_string.py --html=reporttchfgh.html
+# navegador live server extension , correr en go live
+# decorador @pytest.mark.parametrize = arriba de la funcion para que autocomplete con los parametros ingresados pasar parametros a las funciones
+# informe pytest --html=reporte_sexy.html llamar informe en consola
 
 class TestContarVocales(unittest.TestCase):
 
     #@unittest.skip(reason='Por razones de clase')
+    # python -m unittest discover -v -s tests -p "tests_ " * .py
 
-
-    
-    #  Pruebas para contar_vocales
+   
     def test_contar_vocales_texto_normal(self):
         """Prueba contar vocales en un texto común"""
         resultado = contar_vocales(text="hola")
         esperado = 2
         self.assertEqual(first=resultado, second=esperado)
 
-    def test_contar_vocales_sin_vocales(self):
+    @pytest.mark.skip("A la espera de implementacion de HU DATA 3354")
+    @pytest.mark.parametrize(
+         "texto , numero_esperado, mensaje",
+         [
+             ("hola", 12, "Deberia ser 2"),
+             ("murcielago", 5 "soy batman")
+   
+         ]
+           
+    )
+    def test_contar_vocales_sin_vocales(texto , numero_esperado, mensaje):
         """Prueba contar vocales en un texto sin vocales"""
-        resultado = contar_vocales(text="bcdfgh")
-        esperado = 0
-        self.assertEqual(first=resultado, second=esperado)
+        assert contar_vocales(text=texto) == numero_esperado, mensaje
 
     def test_contar_vocales_mayusculas(self):
         """Prueba contar vocales en mayúsculas"""
