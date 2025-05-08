c3943f76041e48ed60bab00f9cebd9cd02abd627
brigith91 <168245239+brigith91@users.noreply.github.com>
2025-05-08 00:46:32 +0000

    cambios

--- Pruebas de aceptacion/main.py
+++ Pruebas de aceptacion/main.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time 
 
-#pip install selenium
-# python -m venv .venv = correr el entorno
-# python .\main.py = correr codigo
+# 3pip install selenium
+# 1 python -m venv .venv = correr el entorno
+# 4 python .\main.py = correr codigo
+# 2./.venv/Scripts/activate 
+
 
 #driver = webdriver.Chrome()
 #driver.get("https://www.google.com")
